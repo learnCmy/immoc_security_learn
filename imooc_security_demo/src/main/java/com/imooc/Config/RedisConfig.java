372a28ade2d58ac953eacfeb8485e9a414d18f3f
cmy <mingyang.cai@kuaiyou.com>
2018-11-16 15:57:34 +0800

    修改utils

---
+++ imooc_security_demo/src/main/java/com/imooc/Config/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.imooc.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @Auther: cmy
+ * @Date: 2018/10/11 11:00
+ * @Description:
+ */
+@Configuration
+public class RedisConfig {
+
+    @Autowired
+    private RedisConnectionFactory factory;
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(factory);
+        return redisTemplate;
+    }
+
+
+}
